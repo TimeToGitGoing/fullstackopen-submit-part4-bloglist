996dd0a17b7a21d0e16fe9f7faacf8da24e1c059
Huda <iamanotherday@gmail.com>
2025-01-30 15:02:10 +0200

    Added MongoDB

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Blog = require('./models/blog')
 const cors = require('cors')
 
-app.use(cors())
-app.use(express.static('dist'))
+let blogs = []
 
-let blogs = [
-    {
-        id: "1",
-        title: "HTML is easy",
-        author: "johnny bravo",
-        url: "www.johnnybravo.com",
-        likes: "19",
-    },
-    {
-        id: "2",
-        title: "Browser can execute javascript",
-        author: "javascript lover",
-        url: "www.javascriptlife.com",
-        likes: "3",
-    },
-    {
-        id: "3",
-        title: "GET and POST are important HTTP Protocols",
-        author: "postperson",
-        url: "www.poster.com",
-        likes: "5",
-    }
-]
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -37,6 +16,17 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -49,59 +39,48 @@ app.get('/', (request, response) => {
 })
   
 app.get('/api/blogs', (request, response) => {
-    response.json(blogs)
+    Blog.find({}).then(blogs => {
+        response.json(blogs)
+    })
 })
 
 app.get('/api/blogs/:id', (request, response) => {
-    const id = request.params.id
-    const blog = blogs.find(blog => blog.id === id)
-    
-    if (blog) {
-        response.json(blog)
-    } else {
-        response.status(400).end()
-    }
+    Blog.findById(request.params.id)
+        .then( blog => {
+            if (blog) {
+                response.json(blog)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
-
-app.delete('/api/blogs/:id', (request, response) => {
-    const id = request.params.id
-    blogs = blogs.filter(blog => blog.id !== id)
-
-    response.status(204).end()
-})
-
-const generateId = () => {
-    const maxId = blogs.length > 0
-        ? Math.max(...blogs.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
 
 app.post('/api/blogs', (request, response) => {
     const body = request.body
 
-    if (!body.title) {
-        return response.status(400).json({ 
-            error: 'title missing' 
-        })
+    if (body.title === undefined) {
+        return response.status(400).json({ error: 'title missing' })
     }
 
-    const blog = {
+    const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
-        id: generateId(),
-    }
+    })
 
-    blogs = blogs.concat(blog)
-
-    response.json(blog)
+    blog.save().then(savedBlog => {
+        response.json(savedBlog)
+    })
 })
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint)  
 
-const PORT = process.env.PORT || 3001
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
