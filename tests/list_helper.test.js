fe902bd8298f0f7ff7e7d653593b2560f784d363
Huda <iamanotherday@gmail.com>
2025-01-31 13:13:17 +0200

    completed 4.4 Helper Functions and Unit Tests, step 2

---
+++ tests/list_helper.test.js
@@ -0,0 +1,67 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '9d7y191dg319dh91dh91dh0',
+      title: 'Why use Javascript at all',
+      author: 'Peter Pan',
+      url: 'https://javascriptlover.com',
+      likes: 8,
+      __v: 0
+    },
+    {
+      _id: '3ada14hrt12x61667128190',
+      title: 'When should you stop sleeping?',
+      author: 'Bob Flincher',
+      url: 'https://sleeptherapy.com',
+      likes: 3,
+      __v: 0
+    }
+  ]
+  
+  test('of empty list is zero', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithThreeBlogs)
+    assert.strictEqual(result, 16)
+  })
+})
